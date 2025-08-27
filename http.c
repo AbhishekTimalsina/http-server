7d2a1ed036fc1ffce01b498a0d488f1ab466fa55
AbhishekTimalsina <hahaguyz546@gmail.com>
2025-08-27 16:52:31 +0545

    creates child process

--- http.c
+++ http.c
@@ -7,6 +7,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <magic.h>
+#include <errno.h>
+#include <signal.h>
+ #include <sys/wait.h>
+
 
 int write_all(int fd, char *data, size_t data_size)
 {
@@ -23,8 +27,18 @@ int write_all(int fd, char *data, size_t data_size)
     return 0;
 }
 
+void sigchild_handler(int signum){
+	 siginfo_t infop;
+	 
+	while(waitid(P_ALL,0,&infop, WEXITED | WNOHANG)==0 && infop.si_pid != 0);
+}
+
 char *getFileExtension(char *filename){
-	char *fileExt= strchr(filename,'.')+1;
+	char *ptr = strchr(filename,'.');
+    if(ptr==NULL) return "html";
+	char *fileExt= ptr;
+	fileExt++;
+
 	return fileExt;
 }
 
@@ -126,13 +140,15 @@ char* parser(char *str,char *field){
 		str+= (valLen+2);
 	}
 	return NULL;
+}
+
+int handleResponse(){
 
 }
 
 int main(){	
 	int BACKLOG=10;
 	int enable=1; // enable SO_REUSEADDR
-	char clientMsg[5000];
 	
 	struct addrinfo hints, *res;
 	memset(&hints,0,sizeof(hints));
@@ -141,30 +157,75 @@ int main(){
 	hints.ai_family= AF_INET;
 	hints.ai_socktype=SOCK_STREAM;
 
-	
-	getaddrinfo(NULL,"3920", &hints,&res);
+	int addrInfoCode= getaddrinfo(NULL,"3920", &hints,&res);
+	if(addrInfoCode!=0){
+		printf("%s",gai_strerror(addrInfoCode));
+		return 1;
+	}
 
 	int sockFd=socket(AF_INET,SOCK_STREAM,0);
 
-	setsockopt(sockFd,SOL_SOCKET,SO_REUSEADDR,&enable,sizeof(enable));
+	if(sockFd==-1){
+		perror("Error in socket");
+		close(sockFd);
 
-	bind(sockFd,res->ai_addr,res->ai_addrlen);
+		return 1;
+	}
 
-	listen(sockFd,BACKLOG);
+	if(setsockopt(sockFd,SOL_SOCKET,SO_REUSEADDR,&enable,sizeof(enable))==-1){
+		perror("Error in setsockopt");
+		return 1;
+	}
+
+
+		if(bind(sockFd,res->ai_addr,res->ai_addrlen)==-1){
+			if(errno== EADDRINUSE){
+				printf("Port is already in Use");
+			}else{
+				perror("Error in bind");
+			}
+			close(sockFd);
+			return 1;
+		}
+
+	if(listen(sockFd,BACKLOG)==-1){
+		perror("Error in socket");
+		close(sockFd);
+		return 1;
+	}
+	
+	struct sigaction act;
+	act.sa_handler= sigchild_handler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_RESTART; 
+	sigaction(SIGCHLD,&act, NULL);
+
 	printf("Listening in port:3920\r\n");
 
 
 	while(1){
 
 		int clientSocketFd=accept(sockFd,NULL,NULL);
-		 int bytesRead=  read(clientSocketFd, clientMsg, 4999);
-		// fflush(NULL);
+			if(clientSocketFd==-1){
+				perror("Error in accept");
+			close(sockFd);
+			return 1;
+			}
+		int pid;
+		if((pid=fork())<0){
+			printf("Error creating fork");
+		}else if(pid==0){
+		close(sockFd);
+		char clientMsg[5000];
+
+
+		int bytesRead=  read(clientSocketFd, clientMsg, 4999);
 		clientMsg[bytesRead] = '\0';
 		char *parsedPath= parser(clientMsg,"path");
-		if(parsedPath==NULL) goto Error;
+		if(parsedPath==NULL) goto parse_error;
 		char *filePath= !strcmp(parsedPath,"/")?"index.html": (parsedPath+1);
 		char *fileContent;
-
+		
 		char fileType[50];
 		if(!strcmp(getFileExtension(filePath),"js")){
 			strcpy(fileType,"application/javascript");
@@ -174,30 +235,41 @@ int main(){
 		}else{
 			strcpy(fileType,getFileType(filePath));
 		}
+		// printf("%s\n",fileType);
 		long fileLength = renderFile(filePath,&fileContent,fileType);
-	
-		Error:
+		
 		if(fileLength==-1){
+			parse_error:
 			char response[]="HTTP/1.1 404\r\nContent-Type:text/html; charset=utf-8\r\n\r\n<html><body>404 File not found</body></html>";
 			write(clientSocketFd,response,strlen(response));
 			close(clientSocketFd);
-			continue;
+			exit(EXIT_FAILURE);
 		}
 		char header[1000];
 
-
 		sprintf(header,"HTTP/1.1 200\r\nContent-Type:%s;\r\nContent-Length:%ld\r\n\r\n",fileType,fileLength);
-
+		
 		write(clientSocketFd,header,strlen(header));
-
+		
 		write_all(clientSocketFd,fileContent,fileLength);
-
+		
 		close(clientSocketFd);
-	}
+
+		exit(EXIT_SUCCESS);
+		
+		}else{
+		
+			//  siginfo_t infop;
+			//  while(waitid(P_ALL,-1,&infop, WEXITED | WNOHANG)==0 && infop.si_pid != 0);
+			close(clientSocketFd);
+			// parent
+		}
+		}
 
 }
 
 
 
-
-
+// Proper Header
+// Learn about C linking and stuff
+// fork
